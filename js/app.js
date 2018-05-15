7e03f5d9d68e1d8d7cd29b45f6abb5f6fad2a969
Ryan Boris <neodynia@icloud.com>
2018-05-15 02:53:14 -0400

    Defined variables to select grid size and color

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+
 // Select color input
+const colorPicked = document.querySelector('#colorPicker').value; 
 // Select size input
+const widthPicked = document.querySelector('#inputWidth').value; 
+const heightPicked = document.querySelector('#inputHeight').value;
 
 // When size is submitted by the user, call makeGrid()
 
