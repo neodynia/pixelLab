fcd45a438211e4a292c5f63dc316cc5f3f4bf868
Ryan Boris <neodynia@icloud.com>
2018-05-15 14:23:00 -0400

    start over

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 
 // Select color input
-const colorPicked = document.querySelector('#colorPicker').value; 
 // Select size input
-const widthPicked = document.querySelector('#inputWidth').value; 
-const heightPicked = document.querySelector('#inputHeight').value;
-
 // When size is submitted by the user, call makeGrid()
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener("click", makeGrid);
 
-function makeGrid() {
-  
-  }
   
