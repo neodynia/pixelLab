fc8990d4847ebca75e46f7d1cd77f26c4822f5de
Ryan Boris <neodynia@icloud.com>
2018-05-19 23:50:09 -0400

    finished

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-//Make a height x width grid of table cells from the user selected values for height and width
+
+(document.getElementById('sizePicker')).addEventListener("submit", function(e) {
+  e.preventDefault();
+  makeGrid();
+  (document.getElementById('button')).style.display = "inline-block";
+});
+
+
+
 function makeGrid() {
     let heightHTML = document.querySelector('#inputHeight');
     let widthHTML = document.querySelector('#inputWidth');
     for (let i = 0; i < heightHTML.value; i++) {
         (document.querySelector('#pixelCanvas')).appendChild(document.createElement('tr'));
+        for (let j = 0; j < widthHTML.value; j++) {
+        (document.querySelector('tr:last-child')).appendChild(document.createElement('td'));
     }
-    for (let j = 0; j < w; j++) {
-        (document.querySelector('tr')).appendChild(document.createElement('td'));
-    }
+  }
 }
+
+
+(document.querySelector('table')).addEventListener("click", function(evt) {
+  evt.target.style.backgroundColor = (document.querySelector('#colorPicker')).value;
+});
+
+(document.getElementById('button')).addEventListener("click", function() {
+  document.location.reload();
+})
